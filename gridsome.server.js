51f1596fe593376692ad5c54a179b082246b1504
matteopaul <matteo.illing@icloud.com>
2019-07-04 15:25:42 +0200

    updated mapper

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,3 +4,62 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require('path');
+const fs = require('fs');
+//const textContentType = require('./dynamicContentTypes/textContentType');
+
+const pagesPath = path.join(__dirname, "pages");
+
+module.exports = function (api) {
+  api.loadSource(store => {
+
+    const contentTypes = {};
+
+    fs.readdirSync(path.join(__dirname, "dynamicContentTypes")).forEach(function(file) {
+      let currentName = file.split('.')[0];
+      contentTypes[currentName] = store.addContentType({
+        typeName: currentName
+      })
+      contentTypes[currentName].addNode(JSON.parse(fs.readFileSync(path.join(__dirname, "dynamicContentTypes") + "/" + file)));
+    })
+
+
+    const pages = store.addContentType({
+      typeName: "PageStructure",
+      route: "/page/:id"
+    });
+
+
+    pages.addNode({
+      id: "0",
+      title: "erste Seite",
+      contents: [store.createReference(contentTypes["imageContentType"].findNode()), store.createReference(contentTypes["textContentType"].findNode())]
+    })
+
+
+    fs.readdirSync(pagesPath).forEach(function(file) {
+
+          let currentData = JSON.parse(fs.readFileSync(path.join(__dirname, "pages") + "/" + file));
+          let items = [];
+
+          currentData.contents.forEach(function(item) {
+            for(let i = 0; i < Object.keys(contentTypes).length; i++) {
+              if(item.type === Object.keys(contentTypes)[i]) {
+                contentTypes[Object.keys(contentTypes)[i]].addNode(item);
+              }
+            }
+          });
+
+          pages.addNode({
+            id: currentData.id,
+            title: currentData.title,
+            contents: items
+          })
+    })
+  })
+  api.createPages(({ createPage }) => {
+    // Use the Pages API here: https://gridsome.org/docs/pages-api
+  })
+
+
+}
